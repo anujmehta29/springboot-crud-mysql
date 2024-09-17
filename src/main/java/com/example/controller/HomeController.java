40772cefc6b6d5a71dbe477e147da33d85c1c2eb
anujmehta29 <anuj.kumarmehta2903@gmail.com>
2024-09-18 01:34:33 +0530

    crud_redisImplementation_registrationFlow

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/") // Maps HTTP GET requests to the root URL
     public String home() {
         return "Welcome to the CRUD Application!";
     }
